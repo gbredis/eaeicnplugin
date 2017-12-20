78ef247f24aea732e601b02423290048d454b874
Георгий Бредис <gbredis@mail.ru>
2017-12-20 18:01:30 +0300

    Backup

---
+++ src/main/resources/ru/eaeconsult/icn/eaeIcnPlugin/WebContent/EAEicnPlugin.js
@@ -0,0 +1,15 @@
+require(["dojo/_base/declare",
+        "dojo/_base/lang",
+        "EAEicnPluginDojo/GlobalInit"],
+    function(declare, lang, GlobalInit) {
+        /**
+         * Use this function to add any global JavaScript methods your plug-in requires.
+         */
+        /*
+            lang.setObject("restartTaskAction", function(repository, items, callback, teamspace, resultSet, parameterMap) {
+
+              * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
+
+            });
+        */
+    });
---
+++ src/main/resources/ru/eaeconsult/icn/eaeIcnPlugin/WebContent/EAEicnPluginDojo/GlobalInit.js
@@ -0,0 +1,24 @@
+/**
+ * Created by BredisGV on 10.08.2016.
+ */
+
+require([
+         "dojo/ready",
+         "dojo/_base/declare",
+         "dojo/_base/lang",
+         "dojo/aspect",
+         "ecm/model/Request",
+         "ecm/model/Desktop"
+         ],
+         function (
+        		 ready,
+        		 declare, 
+        		 lang,
+        		 aspect,
+        		 Request,
+        		 Desktop
+         ) {
+	ready(function () {
+
+	});
+});
